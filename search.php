8c0ed96223a63fc1fa8d39a1bc5ce0fb0c0a6cec
salvob <tholalak@yahoo.it>
2017-05-26 10:56:56 +0200

    save just the data

--- search.php
+++ search.php
@@ -2,6 +2,8 @@
 ini_set('display_errors', 1);
 require_once __DIR__ . "/include/configuration.php";
 
+$_GET = filter_input_array(INPUT_GET, FILTER_SANITIZE_STRING);
+
 $params = [
     "scroll" => "30s",          // how long between scroll requests. should be small!
     "size" => 50,               // how many results *per shard* you want back
@@ -29,13 +31,15 @@ while (isset($response['hits']['hits']) && count($response['hits']['hits']) > 0)
     // You must always refresh your _scroll_id!  It can change sometimes
     $scroll_id = $response['_scroll_id'];
 
+    $complete_response[] = $response['hits']['hits'];
+
     // Execute a Scroll request and repeat
     $response = $client->scroll([
             "scroll_id" => $scroll_id,  //...using our previously obtained _scroll_id
             "scroll" => "30s"           // and the same timeout window
         ]
     );
-    $complete_response[] = $response;
+
 
 }
 print_r(json_encode ($complete_response,JSON_PRETTY_PRINT));
